388011be8036d244c9f94a65acbaa8f01bd5ed08
JohnatanAccourt <johnatan1999accourt@gmail.com>
2021-02-03 21:16:53 -0300

    modulos

---
+++ hello-typescript/main.ts
@@ -0,0 +1,22 @@
+// var minhaVar = 'minha variavel';
+
+
+// function minhaFunc(x,y){
+//     return x + y;
+// }
+
+// let num = 2;
+// const PI = 3.14;
+
+// var numeros = [1, 2, 3];
+// numeros.map((index) => index * 2);
+
+// class Matermatica {
+//     soma(x,y){
+//         return x + y;
+//     }
+// }
+
+// // var n1: string = 'sfdasfdsa';
+// var n1: any = 'fjdkla';
+// n1 = 4;
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+//Decorator, possui metadados
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular 2!</p>
+    `
+})
+
+export class MeuPrimeiroComponent {}
